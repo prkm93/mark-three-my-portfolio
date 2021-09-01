8eff7a46a96e61083f8e84a5e5f7a737ea4ae071
prkm93 <aradshub93@gmail.com>
2021-09-01 20:49:52 +0530

    added projects and details

--- js/data.js
+++ js/data.js
@@ -1,38 +1,52 @@
 export const projectData = [
-    {
-        projectName:'profit-or-loss',
-        date: 'December, 2021',
-        description:'A website which helps user how much profit or loss he/she is making in percentage & absolute value.',
-        liveProjectLink: 'link1',
-        sourceCodeLink:'link2'
-    },
-    {
-        projectName:'palindrome-birthdays',
-        date: 'December, 2020',
-        description:'App which checks whether your birthday is a palindrome birthday or not.',
-        liveProjectLink: 'link1',
-        sourceCodeLink:'link2'
-    },
-    {
-        projectName:'learn-triangles',
-        date: 'December, 2021',
-        description:'A website which helps you to learn about triangles',
-        liveProjectLink: 'link1',
-        sourceCodeLink:'link2'
-    },
-    {
-        projectName:'is-your-birthday-lucky',
-        date: 'December, 2021',
-        description:'A website which helps you to learn about triangles',
-        liveProjectLink: 'link1',
-        sourceCodeLink:'link2'
-    },
+    // {
+    //     projectName:'profit-or-loss',
+    //     date: 'December, 2021',
+    //     description:'A website which helps user how much profit or loss he/she is making in percentage & absolute value.',
+    //     liveProjectLink: 'link1',
+    //     sourceCodeLink:'link2'
+    // },
+    // {
+    //     projectName:'palindrome-birthdays',
+    //     date: 'December, 2020',
+    //     description:'App which checks whether your birthday is a palindrome birthday or not.',
+    //     liveProjectLink: 'link1',
+    //     sourceCodeLink:'link2'
+    // },
+    // {
+    //     projectName:'learn-triangles',
+    //     date: 'December, 2021',
+    //     description:'A website which helps you to learn about triangles',
+    //     liveProjectLink: 'link1',
+    //     sourceCodeLink:'link2'
+    // },
+    // {
+    //     projectName:'is-your-birthday-lucky',
+    //     date: 'December, 2021',
+    //     description:'A website which helps you to learn about triangles',
+    //     liveProjectLink: 'link1',
+    //     sourceCodeLink:'link2'
+    // },
     {
         projectName:'cash-register-manager',
         date: 'December, 2021',
         description:'A cash register manager which helps the user by telling how can he/she return the change to the customer with minimum number of notes.',
         liveProjectLink: 'link1',
         sourceCodeLink:'link2'
+    },
+    {
+        projectName:'a-news-feed-aggregator',
+        date: 'August, 2021',
+        description:'A news feed app where user can browse brief details of specific type of news and then open that news to read the complete news.News is populated from real API.',
+        liveProjectLink: 'https://xboard-by-pradeep.netlify.app/',
+        sourceCodeLink:'https://github.com/prkm93/xboard-crio-exercise-vanillajs'
+    },
+    {
+        projectName:'neogcamp-practice-interview-questions-collection',
+        date: 'July, 2021',
+        description:'A collection of mini apps containing solution of neogcamp\'s interview questions',
+        liveProjectLink: 'https://neogcamp-practice-questions.netlify.app/',
+        sourceCodeLink:'https://github.com/prkm93/neogcamp-practice-interview-questions'
     },
     {
         projectName:'a-general-knowledge-quiz',
